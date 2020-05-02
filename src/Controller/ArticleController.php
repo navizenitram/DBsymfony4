4fa4effdea7d3d6658c96dcf16413cdbd8faaa99
Ivan Martinez <ivan@degustabox.com>
2020-05-02 19:09:39 +0200

    Making so much good progress

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,8 +8,16 @@ use Symfony\Component\HttpFoundation\Response;
 
 class ArticleController
 {
-    public function homepage()
+    public function homepage(): Response
     {
         return new Response("OMG! My first response");
+    }
+
+    public function show($slug): Response
+    {
+        return new Response(sprintf(
+            'Future page to show: %s',
+            $slug
+        ));
     }
 }
