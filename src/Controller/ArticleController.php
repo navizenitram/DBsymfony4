6551e0f16014cefb32ce47d87b04cf73a9c53e4e
Ivan Martinez <ivan@degustabox.com>
2020-05-10 17:40:57 +0200

    Tema 2: Capitulo 3 - Usando la caché

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 
+use Michelf\MarkdownInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -17,7 +19,7 @@ class ArticleController extends AbstractController
         return $this->render('article/homepage.html.twig');
     }
 
-    public function show(string $slug, Environment $twigEnvironment): Response
+    public function show(string $slug, MarkdownInterface $markdown, AdapterInterface $cache): Response
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -25,18 +27,41 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-        //dump($comments, $this);
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.
 
-        //Probando servicios. No es la forma, pero es para ver como funciona. Es lo mismo
-        //que $this->render
-        $html = $twigEnvironment->render('article/show.html.twig',
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
+       //Forma optima y rapida de gestionar la caché.
+        $item           = $cache->getItem('marckdown_' . md5($articleContent));
+        if (!$item->isHit()) {
+            $item->set($markdown->transform($articleContent));
+            $cache->save($item);
+        }
+        $articleContent = $item->get();
+
+        return $this->render('article/show.html.twig',
             [
-                'title'    => ucwords(str_replace('-', ' ', $slug)),
-                'comments' => $comments,
-                'slug'     => $slug,
+                'title'          => ucwords(str_replace('-', ' ', $slug)),
+                'comments'       => $comments,
+                'articleContent' => $articleContent,
+                'slug'           => $slug,
             ]);
 
-        return new Response($html);
+
     }
 
     public function toogleArticleHeart(string $slug, LoggerInterface $logger): response
