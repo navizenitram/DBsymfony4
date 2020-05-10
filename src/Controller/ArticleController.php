546dab4c77955c9c7de7380fb06dbffef24b8ab7
Ivan Martinez <ivan@degustabox.com>
2020-05-10 17:12:43 +0200

    Capítulo 13

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,9 +4,11 @@
 namespace App\Controller;
 
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
@@ -15,7 +17,7 @@ class ArticleController extends AbstractController
         return $this->render('article/homepage.html.twig');
     }
 
-    public function show(string $slug): Response
+    public function show(string $slug, Environment $twigEnvironment): Response
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -25,18 +27,22 @@ class ArticleController extends AbstractController
 
         //dump($comments, $this);
 
-        return $this->render('article/show.html.twig',
+        //Probando servicios. No es la forma, pero es para ver como funciona. Es lo mismo
+        //que $this->render
+        $html = $twigEnvironment->render('article/show.html.twig',
             [
                 'title'    => ucwords(str_replace('-', ' ', $slug)),
                 'comments' => $comments,
                 'slug'     => $slug,
             ]);
+
+        return new Response($html);
     }
 
-    public function toogleArticleHeart(string $slug): response
+    public function toogleArticleHeart(string $slug, LoggerInterface $logger): response
     {
         //TODO
-
+        $logger->info('toggle heart Clickeed');
         return new JsonResponse(['hearts' => rand(1, 100)]);
     }
 }
