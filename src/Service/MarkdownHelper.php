ed6ec6cabac3988d5b298e6262e3c62815984d45
Ivan Martinez <ivan@degustabox.com>
2020-05-18 05:38:39 +0200

    Tema 2: Capitulo 9 - Configurando el autowiring

--- src/Service/MarkdownHelper.php
+++ src/Service/MarkdownHelper.php
@@ -5,25 +5,32 @@ namespace App\Service;
 
 
 use Michelf\MarkdownInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class MarkdownHelper
 {
     private $markdown;
     private $cache;
+    private $logger;
 
 
-    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache)
+    public function __construct(MarkdownInterface $markdown, AdapterInterface $cache, LoggerInterface $logger)
     {
 
         $this->markdown = $markdown;
-        $this->cache = $cache;
+        $this->cache    = $cache;
+        $this->logger   = $logger;
     }
 
     public function parse(string $source): string
     {
+        if (strpos($source, 'bacon') !== false) {
+            $this->logger->info('They are talhing about bacon again!');
+        }
+
         //Forma optima y rapida de gestionar la caché.
-        $item           = $this->cache->getItem('marckdown_' . md5($source));
+        $item = $this->cache->getItem('marckdown_' . md5($source));
         if (!$item->isHit()) {
             $item->set($this->markdown->transform($source));
             $this->cache->save($item);
